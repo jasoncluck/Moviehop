02edc36ed45b20baad2ac59fc27af9484f1f262b
Jason Cluck <jcluck@ycp.edu>
2012-08-22 13:42:58 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moviehop::Application.config.secret_token = '1f108f45a014ee236c836059b93482013d3cbfb6ac2a6945b4fb8c458cb1dbde5d3f8b04e8432e9fba8d0c21b705abc790e5242b5d91a604f3e89254a3dce9cf'
