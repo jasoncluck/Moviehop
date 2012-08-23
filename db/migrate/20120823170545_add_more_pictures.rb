bf213139b2c9a8f2595aff5008d46d3474a6ef8a
Jason Cluck <jcluck@ycp.edu>
2012-08-23 13:53:08 -0400

    migration change

--- db/migrate/20120823170545_add_more_pictures.rb
+++ db/migrate/20120823170545_add_more_pictures.rb
@@ -3,7 +3,7 @@ class AddMorePictures < ActiveRecord::Migration
   	Picture.create!(:movie => Movie.find_by_title("2001: A Space Odyssey"), :address => 'http://upload.wikimedia.org/wikipedia/en/0/0b/2001Style_B.jpg')
   	Picture.create!(:movie => Movie.find_by_title("Chicken Run"), :address => 'http://img.movieberry.com/static/photos/1378/poster.jpg')
 	Picture.create!(:movie => Movie.find_by_title("Chocolat"), :address => 'http://upload.wikimedia.org/wikipedia/en/thumb/0/08/Chocolat_sheet.jpg/220px-Chocolat_sheet.jpg')
-	Picture.create!(:movie => Movie.find_by_title("Raiders of the Lost Ark"), :address => 'http://www.impawards.com/1981/posters/raiders_of_the_lost_ark_ver2.jpg')
+	Picture.create!(:movie => Movie.find_by_title("Raiders of the Lost Ark"), :address => 'http://upload.wikimedia.org/wikipedia/en/4/4b/Raiders.jpg')
 	Picture.create!(:movie => Movie.find_by_title("The Help"), :address => 'http://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Help_poster.jpg/215px-Help_poster.jpg')
 	Picture.create!(:movie => Movie.find_by_title("The Incredibles"), :address => 'http://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Tiposter.jpg/220px-Tiposter.jpg')  	
 	Picture.create!(:movie => Movie.find_by_title("The Terminator"), :address => 'http://upload.wikimedia.org/wikipedia/en/thumb/7/70/Terminator1984movieposter.jpg/220px-Terminator1984movieposter.jpg')
