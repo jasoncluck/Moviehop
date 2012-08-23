765daae0ee0d68b98dfe2670c0a8e16118c7f974
Jason Cluck <jcluck@ycp.edu>
2012-08-23 13:49:48 -0400

    migration change

--- db/migrate/20120823161605_create_pictures.rb
+++ db/migrate/20120823161605_create_pictures.rb
@@ -1,7 +1,6 @@
 class CreatePictures < ActiveRecord::Migration
   def up
-  	drop_table :pictures
-  	create_table :pictures do |t|
+    	create_table :pictures do |t|
   		t.references :movie
   		t.string :address
   		t.text :description
