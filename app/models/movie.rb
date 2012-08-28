cb7293707bdeb8069df3d6143d4760411c085582
Jason Cluck <jcluck@ycp.edu>
2012-08-28 11:52:50 -0400

    fix validation typo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ActiveRecord::Base
 
 	attr_accessible :title, :rating, :release_date, :tagline, :trailer, :picture1, :picture2
 	validates_presence_of :trailer, :picture1, :picture2, :title
-	validates_uniquness_of :title
+	validates_uniqueness_of :title
  	
 end
